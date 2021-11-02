b5fb41c789a7435b6cfb3267664766fd98b701aa
metin.aralioglu <metin.aralioglu@ajanspress.com.tr>
2021-11-02 17:33:09 +0300

    t

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,11 +1,30 @@
+using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
 namespace PlatformService.Controllers
 {
+  [Route("api/platforms")]
+  [ApiController]
   public class PlatformsController : ControllerBase
   {
+    private readonly IPlatformRepo _repository;
+    private readonly IMapper _mapper;
+
     public PlatformsController(IPlatformRepo repository, IMapper mapper)
     {
+      _repository = repository;
+      _mapper = mapper;
+    }
 
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+      System.Console.WriteLine("--> Getting Platforms ....");
+      var platformItem = _repository.GetAllPlatforms();
+      return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
     }
   }
 }
